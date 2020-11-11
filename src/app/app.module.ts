660051db71ac92f7f791166bbf16c93e201ae691
arjunkhetia <arjunkhetia@gmail.com>
2020-11-11 19:36:12 +0530

    Output Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { NgBsFormBuilderModule } from 'ng-bs-form-builder';
-import { NgBsFormBuilderModule } from '../../projects/ng-bs-form-builder/src/lib/ng-bs-form-builder.module';
+import { NgBsFormBuilderModule } from 'ng-bs-form-builder';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
