2adae22732a07fc802d30b503bec5451301e0818
arjunkhetia <arjunkhetia@gmail.com>
2020-11-11 14:38:11 +0530

    Range Done

--- projects/ng-bs-form-builder/src/lib/fields/range/range.component.ts
+++ projects/ng-bs-form-builder/src/lib/fields/range/range.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'lib-range',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RangeComponent implements OnInit {
 
+  @Input() field: any = {};
+  @Input() form: FormGroup;
+  public step: any = '';
+
+  get isValid() { return this.form.controls[this.field.name].valid; }
+  get isDirty() { return this.form.controls[this.field.name].dirty; }
+  get isTouched() { return this.form.controls[this.field.name].touched; }
+  get isErrors() { return this.form.controls[this.field.name].errors; }
+
   constructor() { }
 
   ngOnInit(): void {
+    this.step = this.field.value;
+  }
+
+  stepChange(event: any) {
+    this.step = event.target.value;
   }
 
 }
