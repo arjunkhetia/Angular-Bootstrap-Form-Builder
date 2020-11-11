988749d7e29bbfb0a19dc5ea4bc068f6fb2e83ef
arjunkhetia <arjunkhetia@gmail.com>
2020-11-11 14:36:55 +0530

    Radio Done

--- projects/ng-bs-form-builder/src/lib/fields/radio/radio.component.ts
+++ projects/ng-bs-form-builder/src/lib/fields/radio/radio.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'lib-radio',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./radio.component.scss']
 })
 export class RadioComponent implements OnInit {
+
+  @Input() field: any = {};
+  @Input() form: FormGroup;
+
+  get isValid() { return this.form.controls[this.field.name].valid; }
+  get isDirty() { return this.form.controls[this.field.name].dirty; }
+  get isTouched() { return this.form.controls[this.field.name].touched; }
+  get isErrors() { return this.form.controls[this.field.name].errors; }
 
   constructor() { }
 
