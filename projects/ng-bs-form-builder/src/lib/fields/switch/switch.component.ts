ebbe801fa4c1674f9c461028fc05452d56632c43
arjunkhetia <arjunkhetia@gmail.com>
2020-11-11 14:39:54 +0530

    Switch Done

--- projects/ng-bs-form-builder/src/lib/fields/switch/switch.component.ts
+++ projects/ng-bs-form-builder/src/lib/fields/switch/switch.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'lib-switch',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchComponent implements OnInit {
 
+  @Input() field: any = {};
+  @Input() form: FormGroup;
+  public isEmpty = true;
+
+  get isValid() { return this.form.controls[this.field.name].valid; }
+  get isDirty() { return this.form.controls[this.field.name].dirty; }
+  get isTouched() { return this.form.controls[this.field.name].touched; }
+  get isErrors() { return this.form.controls[this.field.name].errors; }
+
   constructor() { }
 
   ngOnInit(): void {
+    this.checkIsEmpty();
+  }
+
+  checkChecked(key, value) {
+    if (value[key]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  onCheckChange(event) {
+    if (this.field.value[event.target.value]) {
+      this.field.value[event.target.value] = false;
+    } else {
+      this.field.value[event.target.value] = true;
+    }
+    this.checkIsEmpty();
+  }
+
+  checkIsEmpty() {
+    for (let val in this.field.value) {
+      if (this.field.value[val]) {
+        this.isEmpty = false;
+        break;
+      } else {
+        this.isEmpty = true;
+      }
+    }
   }
 
 }
