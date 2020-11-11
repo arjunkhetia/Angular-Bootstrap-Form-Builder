62a9d687ecf7f7d712f6cf30670c83768321ba63
arjunkhetia <arjunkhetia@gmail.com>
2020-11-11 13:52:47 +0530

    Field Builder Done

--- projects/ng-bs-form-builder/src/lib/field-builder/field-builder.component.ts
+++ projects/ng-bs-form-builder/src/lib/field-builder/field-builder.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-field-builder',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./field-builder.component.scss']
 })
 export class FieldBuilderComponent implements OnInit {
+
+  @Input() field: any;
+  @Input() form: any;
+
+  get isValid() { return this.form.controls[this.field.name].valid; }
+  get isDirty() { return this.form.controls[this.field.name].dirty; }
+  get isTouched() { return this.form.controls[this.field.name].touched; }
+  get isErrors() { return this.form.controls[this.field.name].errors; }
 
   constructor() { }
 
